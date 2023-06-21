e43aa2c0a3a179e91aec7151f0f35815627bfb1a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-06-21 00:15:44 -0400

    added content to hero

--- Portfolio/src/Components/Hero.tsx
+++ Portfolio/src/Components/Hero.tsx
@@ -1,10 +1,17 @@
-
+import { AiFillHtml5 } from "react-icons/ai";
+import { DiCss3, DiJavascript, DiReact } from "react-icons/di";
 
 const Hero = () => {
   return (
-    <div>
-        <p>GREGORY STARNES</p>
-        <p>FRONT-END WEB DEVELOPER</p>
+    <div className="flex flex-col gap-2 items-center w-full mt-[300px] ">
+        <p className="text-6xl md:text-7xl lg:text-8xl text-[#dda74f] font-bold">GREGORY STARNES</p>
+        <p className="text-3xl md:text-4xl lg:text-5xl">FRONT-END WEB DEVELOPER</p>
+        <ul className="flex lg:mt-6 md:mt-6 md:gap-6 lg:gap-8">
+            <li><AiFillHtml5 size={60} className="md:scale-125 lg:scale-150"/></li>
+            <li><DiCss3 size={60} className="md:scale-125 lg:scale-150" /></li>
+            <li><DiJavascript size={60} className="md:scale-125 lg:scale-150" /></li>
+            <li><DiReact size={60} className="md:scale-125 lg:scale-150" /></li>
+        </ul>
     </div>
   )
 }
