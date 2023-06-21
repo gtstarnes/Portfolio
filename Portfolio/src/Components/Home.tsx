12158d90c479865394e1856aeadb1f57ad680643
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-06-21 01:45:57 -0400

    added darkMode Styles

--- Portfolio/src/Components/Home.tsx
+++ Portfolio/src/Components/Home.tsx
@@ -1,16 +1,18 @@
 import { AiFillHtml5 } from "react-icons/ai";
 import { DiCss3, DiJavascript, DiReact } from "react-icons/di";
+import { SiTailwindcss } from "react-icons/si"
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-2 items-center w-full mt-[300px] ">
-        <p className="text-6xl md:text-7xl lg:text-8xl text-[#dda74f] font-bold">GREGORY STARNES</p>
+    <div className="flex flex-col gap-2 items-center min-w-[600px] w-full mt-[300px] dark:bg-[#000300] dark:text-white">
+        <p className="text-6xl flex-nowrap md:text-7xl lg:text-8xl text-[#dda74f] font-bold">GREGORY STARNES</p>
         <p className="text-3xl md:text-4xl lg:text-5xl">FRONT-END WEB DEVELOPER</p>
         <ul className="flex lg:mt-6 md:mt-6 md:gap-6 lg:gap-8">
-            <li><AiFillHtml5 size={60} className="md:scale-125 lg:scale-150"/></li>
-            <li><DiCss3 size={60} className="md:scale-125 lg:scale-150" /></li>
-            <li><DiJavascript size={60} className="md:scale-125 lg:scale-150" /></li>
-            <li><DiReact size={60} className="md:scale-125 lg:scale-150" /></li>
+            <li><AiFillHtml5 size={60} className="md:scale-125 lg:scale-150 dark:text-[#dda74f]"/></li>
+            <li><DiCss3 size={60} className="md:scale-125 lg:scale-150 dark:text-[#dda74f]" /></li>
+            <li><DiJavascript size={60} className="md:scale-125 lg:scale-150 dark:text-[#dda74f]" /></li>
+            <li><DiReact size={60} className="md:scale-125 lg:scale-150 dark:text-[#dda74f]" /></li>
+            <li><SiTailwindcss size={60} className="md:scale-125 lg:scale-150 dark:text-[#dda74f]" /></li>
         </ul>
     </div>
   )
--- Portfolio/src/Components/NavBar.tsx
+++ Portfolio/src/Components/NavBar.tsx
@@ -1,33 +1,45 @@
 import { AiFillLinkedin, AiFillGithub, AiOutlineMenu, AiOutlineClose, AiFillMail } from 'react-icons/ai';
-import { useState } from 'react';
+import { LuSunMoon } from 'react-icons/lu';
+import { useState, useEffect } from 'react';
 
 const NavBar = () => {
     const [menu, setMenu] = useState<boolean>(false);
-    const handleMenuClick = () => {
-        setMenu(!menu);
+    const [darkMode, setDarkMode] = useState<boolean>(false);
+    const handleClick = () => {
+        setMenu(current => !current);
     }
-
+    const handleDarkMode = () => {
+        const html = document.documentElement;
+        setDarkMode(!darkMode)
+        if (darkMode) {
+            html.classList.remove("dark");
+        } else {
+            html.className = "dark";
+        }
+       
+    }
   return (
     <>
-    
-    
-    <nav className="flex justify-between w-full border-b-2 drop-shadow-md pb-4">
+    <nav className="flex justify-between w-full border-b-2 dark:border-[#dda74f] drop-shadow-md pb-4">
         <p className="text-[#dda74f] font-bold ml-4 mt-2 text-xl">GREGORY STARNES PORTFOLIO</p>
-        <ul className="sm:flex gap-4 hidden mt-2 pr-4 text-lg">
+        <ul className="sm:flex sm:fixed sm:right-10 gap-4 hidden mt-2 pr-4 text-lg">
             <li>Home</li>
             <li>About</li>
             <li>Projects</li>
         </ul>
+        <div>
+            <LuSunMoon size={20} className="fixed top-3 right-[50px] sm:right-[20px]" onClick={handleDarkMode} />
+        </div>
         <div className="block sm:hidden">
-        {!menu ? <AiOutlineMenu className="mr-4 mt-[13px]" onClick={handleMenuClick} size={20} /> : <AiOutlineClose className="mr-4 mt-[13px]" onClick={handleMenuClick} size={20} />}
+        {!menu ? <AiOutlineMenu className="mr-4 mt-[13px]" onClick={handleClick} size={20} /> : <AiOutlineClose className="mr-4 mt-[13px]" onClick={handleClick} size={20} />}
         </div>
     </nav>
     
-    <nav className={menu ? "bg-white border-r-2 fixed h-full top-0 w-[60%] ease-in-out duration-200" : "fixed left-[-100%]" }>
+    <nav className={menu ? "bg-white dark:bg-[#000300] border-r-2 fixed h-full top-0 w-[60%] ease-in-out duration-200" : "fixed left-[-100%]" }>
         <p className="text-[#dda74f] font-bold ml-4 mt-2 text-xl"></p>
         <ul className="flex flex-col gap-4 ml-4 mt-10 text-lg w-[80%]">
-            <li className="border-b-2 pb-4">Home</li>
-            <li className="border-b-2 pb-4">About</li>
+            <li className="border-b-2 dark:border-[#dda74f] pb-4">Home</li>
+            <li className="border-b-2 dark:border-[#dda74f] pb-4">About</li>
             <li>Projects</li>
         </ul>
         <p className="text-lg font-bold mb-2 ml-4 mt-20">CONTACT</p>
