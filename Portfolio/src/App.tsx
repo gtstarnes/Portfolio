37b070fcb15e5a28dd9c2caecc68e8737c40e430
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-06-21 00:40:39 -0400

    updated component name

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import NavBar from './Components/NavBar';
-import Hero from './Components/Hero';
+import Home from './Components/Home';
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <NavBar />
-    <Hero />
+    <Home />
     </>
   )
 }
