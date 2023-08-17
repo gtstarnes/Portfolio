faf95add8eb36158be07d64b98a5a71e5560af7a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-16 20:02:35 -0400

    updated with border

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 import NavBar from './Components/NavBar';
-import Home from './Components/Home';
+import Home from './Pages/Home';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <NavBar />
-    <Home />
-    </>
+
+    <div className="flex flex-col gap-2 items-center text-center self-center max-w-[1500px] mt-[200px] dark:bg-[#000300] 
+  dark:text-white border-8 border-[#dda74f] py-40 px-4 md:mx-auto mx-10">
+      <NavBar />
+      <Home />
+    </div>
+
   )
 }
 
