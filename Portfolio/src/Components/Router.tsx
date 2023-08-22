5f67e5aa855e83034d521ff040801b6a718517e5
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-21 22:20:09 -0400

    initial commit

---
+++ Portfolio/src/Components/Router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const Router = () => {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            
+        }
+    ])
+  return (
+    <div>Router</div>
+  )
+}
+
+export default Router
