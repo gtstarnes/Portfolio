43e3f3f7dc40bb794d053aa61af3c9f134ad2712
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-21 22:30:09 -0400

    set up router

--- Portfolio/src/Components/Router.tsx
+++ Portfolio/src/Components/Router.tsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App";
+import About from "../Pages/About";
+import Home from "../Pages/Home";
 
 const Router = () => {
     const router = createBrowserRouter([
         {
             path: "/",
-            
+            element: <App />,
+            children: [
+                {index: true, element: <Home />},
+                {path: "about", element: <About />}
+            ]
         }
     ])
-  return (
-    <div>Router</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default Router
