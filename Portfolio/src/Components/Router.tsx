f8488209800679d8fea9faefd1acc814830e639e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-21 23:31:00 -0400

    added missing comma

--- Portfolio/src/Components/Router.tsx
+++ Portfolio/src/Components/Router.tsx
@@ -11,7 +11,7 @@ const Router = () => {
             element: <App />,
             children: [
                 {index: true, element: <Home />},
-                {path: "about", element: <About />}
+                {path: "about", element: <About />},
                 {path: "projects", element: <Projects />}
             ]
         }
