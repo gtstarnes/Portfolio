7f4ac3f4a118b076b6604d544a50dd0ca323c257
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-08-21 23:27:04 -0400

    added Outlet component

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
+
 import NavBar from './Components/NavBar';
 import Home from './Pages/Home';
-
+import About from './Pages/About';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
 
     <div className="flex flex-col gap-2 items-center text-center self-center max-w-[1500px] mt-[200px] dark:bg-[#000300] 
   dark:text-white border-8 border-[#dda74f] py-40 px-4 md:mx-auto mx-10">
       <NavBar />
-      <Home />
+      <Outlet />
     </div>
 
   )
